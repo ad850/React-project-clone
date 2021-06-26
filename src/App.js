c02ac3875b58f50c2157da338ce1ffc931a35693
aditya <aditya.sharma9827.as@gmail.com>
2021-06-26 20:26:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Card from './Card';
+import List from './List';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+    return (
+
+
+        <>
+            <h1 className="heading">Best Netflix Series of All Time</h1>
+
+            {
+                List.map((val) => {
+                    return (
+
+                        <Card
+                            image={val.image}
+                            title={val.title}
+                            sname={val.sname}
+                            link={val.link}
+                        />
+
+                    )
+
+                })
+            }
+
+        </>
+
+
+    )
+
+
 }
 
 export default App;
---
+++ src/List.jsx
@@ -0,0 +1,75 @@
+
+
+
+
+const List = [
+
+
+    {
+        link: "https://www.netflix.com/in/title/70276033",
+        sname: "orphan black",
+        image: "https://picsum.photos/id/870/200/300?grayscale&blur=2",
+        title: "A netflix original series",
+    },
+
+
+    {
+        link: "https://www.netflix.com/in/title/70276033",
+        sname: "Inout",
+        image: "https://picsum.photos/200/300",
+        title: "A netflix original series",
+    },
+
+
+    {
+        link: "https://www.netflix.com/in/title/70276033",
+        title: "A netflix original series",
+        image: "https://picsum.photos/200/300/?blur",
+        sname: "Traveller",
+    },
+
+
+    {
+        link: "https://www.netflix.com/in/title/80105699",
+        title: "A netflix original series",
+        image: "https://picsum.photos/200/300?grayscale",
+        sname: "Spit-out"
+    },
+
+
+
+    {
+        link: "https://www.netflix.com/in/title/80991826",
+        title: "A netflix original series",
+        image: "https://picsum.photos/seed/picsum/200/300",
+        sname: "jumangi",
+    },
+
+]
+export default List;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
